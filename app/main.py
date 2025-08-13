136fd73a3b19e2e0c65f9cd57a4c8aef62c022b9
Kingvics <okoromivictorsunday@gmail.com>
2025-08-14 00:00:57 +0100

    fixed render deployment issue

--- app/main.py
+++ app/main.py
@@ -3,15 +3,8 @@ from . import models, database
 from .router import posts, users, auth, vote
 from .config import settings
 from fastapi.middleware.cors import CORSMiddleware
-import os
-import uvicorn
 
-print(settings.database_username)
-
-# models.Base.metadata.create_all(database.engine)
-
-
-app =  FastAPI()
+app = FastAPI()
 
 origins = ["*"]
 
@@ -23,19 +16,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 @app.get('/')
 def index():
-    return 'hello world'
+    return {"message": "hello world"}
+
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(posts.router)
 app.include_router(vote.router)
-
-
-
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
