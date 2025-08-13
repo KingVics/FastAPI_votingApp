e4e1ec3f851c53deebbffb269847d9e089c20a82
Kingvics <okoromivictorsunday@gmail.com>
2025-08-14 00:10:39 +0100

    fixed render deployment issue2

--- app/database.py
+++ app/database.py
@@ -33,7 +33,7 @@ def get_db():
 
 while True:
     try:
-        con = psycopg2.connect(host="localhost", database="voteApp", user="postgres", password="admin", 
+        con = psycopg2.connect(host=settings.database_hostname, database=settings.database_name, user=settings.database_username, password=settings.database_password, 
                                cursor_factory=RealDictCursor)
         cursor = con.cursor()
         print("Connected to db")
